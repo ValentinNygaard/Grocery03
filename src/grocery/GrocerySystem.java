3b4283afceaa2cecf2577e50cf95f2f528e24503
ValentinNygaard <rene930d@stud.kea.dk>
2018-09-04 14:07:06 +0200

    hej

--- src/grocery/GrocerySystem.java
+++ src/grocery/GrocerySystem.java
@@ -1,6 +1,8 @@
 package grocery;
 
 public class GrocerySystem {
+
+    int i = 0;
     //GroceryItemOrderHandler.initGroceryItemList();
     //GroceryItemOrderHandler.printGroceryItemList();
     public void getTotal(){
