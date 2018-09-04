036a23cccdf907f35c9a8b978c8811183a620516
ValentinNygaard <rene930d@stud.kea.dk>
2018-09-04 13:51:05 +0200

    hej

--- src/grocery/GroceryItemOrderHandler.java
+++ src/grocery/GroceryItemOrderHandler.java
@@ -5,47 +5,48 @@ import java.util.Scanner;
 
 public class GroceryItemOrderHandler {
 
-    FileHandler fileIO = new FileHandler();
-
     // Making ArrayList for objects
 
     static ArrayList<GroceryItemOrder> groceryItemList = new ArrayList<GroceryItemOrder>();
 
     // Initialising
 
-    public void initGroceryItemList()
+    public static void initGroceryItemList()
     {
-        Scanner input = new Scanner(fileIO.readFile("grocery/GroceryItemData.txt"));
+        FileHandler file = new FileHandler();
+
+        Scanner input = new Scanner(file.readFile("groceryItemData.txt"));
 
         while (input.hasNextLine())
         {
             String fileLine = input.nextLine();
             Scanner item = new Scanner(fileLine);
             int quantity = item.nextInt();
-            int pricePrItem = item.nextInt();
-            String name = item.next();
+            double pricePrItem = item.nextDouble();
+            String nameTemp = item.nextLine();
+            String name = nameTemp.substring(1,nameTemp.length());
 
-            groceryItemList.add(new GroceryItemOrder(quantity, pricePrItem, name));
+            groceryItemList.add(new GroceryItemOrder(quantity,pricePrItem,name));
         }
     }
 
     // Show list of all reservations
 
-    public void printGroceryItemList()
+    public static void printGroceryItemList()
     {
         System.out.println("GROCERY ITEMS");
         System.out.println();
-        System.out.println("#  Name             Price pr item");
-        System.out.println("-------------------------------------------------------------------");
+        System.out.println("  #  Name                     Price pr item");
+        System.out.println("---------------------------------------------");
         for (int i=0; i<groceryItemList.size(); i++)
         {
-            System.out.printf("%5d",(i+1));
-            System.out.printf("%20s",groceryItemList.get(i).getName());
-            System.out.printf("%5d",groceryItemList.get(i).getPricePrUnit());
+            System.out.printf("%3d",(i+1));
+            System.out.printf("  ");
+            System.out.printf("%-30s",groceryItemList.get(i).getName());
+            System.out.printf("%8.2f",groceryItemList.get(i).getPricePrUnit());
             System.out.println();
         }
         System.out.println();
     }
-
 
 }
