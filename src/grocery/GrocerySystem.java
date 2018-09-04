2523d7ff59486ac25483ea7f3ba0ce749e476811
ValentinNygaard <rene930d@stud.kea.dk>
2018-09-04 14:09:17 +0200

    hej

--- src/grocery/GrocerySystem.java
+++ src/grocery/GrocerySystem.java
@@ -5,8 +5,8 @@ public class GrocerySystem {
     int i = 0;
     //GroceryItemOrderHandler.initGroceryItemList();
     //GroceryItemOrderHandler.printGroceryItemList();
-    public void getTotal(){
-        //code here
+    public void goShopping(){
+        GroceryItemOrderHandler.printGroceryItemList();
     }
 
 }
