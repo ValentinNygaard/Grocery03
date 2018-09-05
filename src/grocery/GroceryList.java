44a242ade02549bc74d8efa81f3717efe0399b44
ValentinNygaard <rene930d@stud.kea.dk>
2018-09-05 10:12:45 +0200

    hej

--- src/grocery/GroceryList.java
+++ src/grocery/GroceryList.java
@@ -2,20 +2,20 @@ package grocery;
 
 public class GroceryList {
 
-    GroceryItemOrder shoppingList[] = new GroceryItemOrder[10];
-    int shoppingListCounter = 0;
+    static GroceryItemOrder shoppingList[] = new GroceryItemOrder[10];
+    static int shoppingListCounter = 0;
 
     public void addGroceryItemOrder(GroceryItemOrder newItem){
         shoppingList[shoppingListCounter] = newItem;
         shoppingListCounter ++;
     }
 
-    public void getTotalCost(){
+    public static void getTotalCost(){
         double totalCost = 0;
         System.out.println("GROCERY SHOPPING BASKET");
         System.out.println();
-        System.out.println("  #  Name                     Item Price  Quantity  Price");
-        System.out.println("-----------------------------------------------------------");
+        System.out.println("  #  Name                        Item Price  Quantity   Price");
+        System.out.println("---------------------------------------------------------------");
         for (int i=0; i<shoppingListCounter; i++)
         {
             totalCost += shoppingList[i].getCost();
@@ -23,13 +23,14 @@ public class GroceryList {
             System.out.printf("  ");
             System.out.printf("%-30s",shoppingList[i].getName());
             System.out.printf("%8.2f",shoppingList[i].getPricePrUnit());
-            System.out.printf("%3d",shoppingList[i].getQuantity());
+            System.out.printf("%7d",shoppingList[i].getQuantity());
             System.out.printf("%8.2f",shoppingList[i].getCost());
             System.out.println();
         }
-        System.out.println();
-        System.out.println("-----------------------------------------------------------");
-        System.out.printf("%50d",totalCost);
+        //System.out.println();
+        System.out.println("---------------------------------------------------------------");
+        System.out.print("Total cost");
+        System.out.printf("%55.2f",totalCost);
         System.out.println();
     }
 }
--- src/grocery/GrocerySystem.java
+++ src/grocery/GrocerySystem.java
@@ -2,11 +2,47 @@ package grocery;
 
 public class GrocerySystem {
 
-    int i = 0;
-    //GroceryItemOrderHandler.initGroceryItemList();
-    //GroceryItemOrderHandler.printGroceryItemList();
     public static void goShopping(){
-        GroceryItemOrderHandler.printGroceryItemList();
+
+        System.out.println("WELCOME TO GROCERY SHOPPING");
+        System.out.println("---------------------------------------------");
+        System.out.println();
+
+        GroceryItemOrderHandler.initGroceryItemList();
+
+        GroceryList shoppingCart = new GroceryList();
+
+        boolean repeatMenu = true;
+        do
+        {
+            GroceryItemOrderHandler.printGroceryItemList();
+            System.out.println();
+            System.out.print("Enter #id of item to add to shopping cart - Enter 0 to check out: ");
+            int menuChoice = InputHandler.getInt(0,4,"Only numbers allowed - Try again: ","Out of range - Try again: "); // Get user input
+            System.out.println();
+
+            // Activate user selected menu
+
+            if (menuChoice == 0)
+            {
+                repeatMenu = false;
+            }
+            else
+            {
+                System.out.print("Enter the quantity of the selected item: ");
+                int quantityChoice = InputHandler.getInt(0,99,"Only numbers allowed - Try again: ","Out of range - Try again: "); // Get user input
+                System.out.println();
+                GroceryItemOrderHandler.groceryItemList.get(menuChoice-1).setQuantity(quantityChoice);
+                shoppingCart.addGroceryItemOrder(GroceryItemOrderHandler.groceryItemList.get(menuChoice-1));
+                GroceryList.getTotalCost();
+            }
+        }
+        while (repeatMenu);
+        System.out.print("CHECKOUT");
+        System.out.print();
+        System.out.print("Enter the quantity of the selected item: ");
+
+
     }
 
 }
